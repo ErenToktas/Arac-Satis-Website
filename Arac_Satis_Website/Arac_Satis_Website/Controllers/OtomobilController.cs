1999f473bd5d4164fa87e8ffdf87431dba134822
ErenToktas <erentoktas@users.noreply.github.com>
2020-09-01 23:40:52 +0300

    update
    Database
    Adminlayout
    bootstrap themes

---
+++ Arac_Satis_Website/Arac_Satis_Website/Controllers/OtomobilController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Arac_Satis_Website.Models.entity;
+namespace Arac_Satis_Website.Controllers
+{
+    public class OtomobilController : Controller
+    {
+        // GET: Otomobil
+        Oto_SatisEntities db = new Oto_SatisEntities();
+        public ActionResult Index()
+        {
+            var ss = db.Ilan.ToList();
+            return View(ss);
+        }
+    }
+}
---
+++ Arac_Satis_Website/Arac_Satis_Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arac_Satis_Website.MvcApplication" Language="C#" %>
